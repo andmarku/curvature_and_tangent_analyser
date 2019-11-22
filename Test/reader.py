31aa9eb96f383bb0a737d6e05d4be1b5cdb41ade
snusstofilen <hannouch@student.chalmers.se>
2019-11-22 07:29:42 +0100

    Corrected a small mistake.

--- Test/reader.py
+++ Test/reader.py
@@ -19,13 +19,11 @@ dataset = dataset.reshape(dim, order='F')
 nonzero_element_coordinates = dataset.nonzero()
 z, x, y = nonzero_element_coordinates
 
-pdb.set_trace()
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z, zdir='z', c= 'red')
+ax.scatter(x, y, z, zdir='z', c= 'red', s=1)
 plt.show()
 
 pdb.set_trace()
