e46bed9f7ca8dafb339f1e2f268ff70dd5a729ef
snusstofilen <hannouch@student.chalmers.se>
2019-11-27 16:41:00 +0100

    changed some stuff

--- Test/gaussianSmoothing.py
+++ Test/gaussianSmoothing.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.ndimage import gaussian_filter1d
 from random import randint
 import random
-import readerMethod
 
 # Compute derivates with convolution
 def convolution(input, sig = 1):
@@ -10,8 +9,9 @@ def convolution(input, sig = 1):
     Iy = gaussian_filter1d(input, sigma=sig, order = 1, axis = 1, mode='constant', truncate = 3*sig)
     Iz = gaussian_filter1d(input, sigma=sig, order = 1, axis = 2, mode='constant', truncate = 3*sig)
     return Ix, Iy, Iz
-
+'''
 if __name__== "__main__":
   input = readerMethod.readVTK('input.vtk')[0].astype('float')
   A = convolution(input, sig = 2)
   print(A)
+ '''
--- Test/readerMethod.py
+++ Test/readerMethod.py
@@ -1,7 +1,12 @@
-import numpy
+import numpy as np
+import scipy.linalg as sp
 import pdb
 from vtk import vtkStructuredPointsReader
 from vtk.util import numpy_support as VN
+from GST0 import calculateGST0
+from gaussianSmoothing import convolution
+import matplotlib.pyplot as plt
+from physt import special
 
 
 def readVTK(filename = 'input.vtk'):
@@ -32,3 +37,62 @@ def readVTK(filename = 'input.vtk'):
     # plt.show()
 
     # pdb.set_trace()
+
+if __name__ == '__main__':
+
+    d, x, y, z = readVTK()
+    Ix, Iy, Iz = convolution(d)
+
+    A = np.zeros(d.shape + (3,))
+
+    A[:,:,:,0] = Ix
+    A[:,:,:,1] = Iy
+    A[:,:,:,2] = Iz
+
+    GST = calculateGST0(A, 1)
+
+    x_coord = np.zeros((200, 200, 200))
+    y_coord = np.zeros((200, 200, 200))
+    z_coord = np.zeros((200, 200, 200))
+
+    for (i, j, k), m in np.ndenumerate(GST[:,:,:,0,0]):
+        #eigs, vecs = sp.eig(GST[i,j,k,:,:])
+        eigs, vecs = np.linalg.eig(GST[i,j,k,:,:])
+        index = np.argmin(abs(eigs))
+        x_coord[i,j,k] = vecs[0, index]
+        y_coord[i,j,k] = vecs[1, index]
+        z_coord[i,j,k] = vecs[2, index]
+
+    data = np.zeros((200*200*200, 3))
+
+    x_coord = x_coord.flatten()
+    y_coord = y_coord.flatten()
+    z_coord = z_coord.flatten()
+
+    data[:, 0] = x_coord
+    data[:, 1] = y_coord
+    data[:, 2] = z_coord
+
+    spherical = np.zeros((200*200*200, 3))
+    hxy = np.hypot(data[:,0], data[:,1])
+    r = np.hypot(hxy, data[:,2])
+    el = np.arctan2(data[:,2], hxy)
+    az = np.arctan2(data[:,1], data[:,0])
+
+    #import pdb; pdb.set_trace()
+
+    spherical[:,0] = r
+    spherical[:,1] = el
+    spherical[:,2] = az
+
+    h = special.spherical_histogram(spherical)
+    globe = h.projection("theta", "phi")
+    # globe.plot()
+    globe.plot.globe_map(density=True, figsize=(7, 7), cmap="rainbow")
+    globe.plot.globe_map(density=False, figsize=(7, 7))
+    plt.show()
+
+    #import pdb; pdb.set_trace()
+
+    #plt.hist(eigenvalues)
+    #plt.show()
