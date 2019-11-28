0e40839a260025b4339580c7aa6e58565abadbd5
snusstofilen <hannouch@student.chalmers.se>
2019-11-28 13:47:23 +0100

    More general (always nice).

--- Test/GST0.py
+++ Test/GST0.py
@@ -4,7 +4,7 @@ from scipy.ndimage import gaussian_filter
 ''' Calculates the gradient structure tensor '''
 def calculateGST0(partials, std):
 
-    GST = np.zeros(partials.shape + (3,))
+    GST = np.zeros(partials.shape + (partials.shape[3],))
 
     for (i, j), _ in np.ndenumerate(GST[0,0,0,:,:]):
         structure = partials[:,:,:,i] * partials[:,:,:,j]
