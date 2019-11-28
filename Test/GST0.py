ab2a84f3ddd279c6cf42bde7075b43a88ffdb40c
snusstofilen <hannouch@student.chalmers.se>
2019-11-28 13:50:54 +0100

    comments on input and output

--- Test/GST0.py
+++ Test/GST0.py
@@ -1,7 +1,15 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 
-''' Calculates the gradient structure tensor '''
+''' Calculates the gradient structure tensor
+
+Input is on the form:
+    numpy array: partials = (200, 200, 200, 3)
+    real value: std
+
+Output is on the form:
+    numpy array: GST = (200, 200, 200, 3, 3)
+'''
 def calculateGST0(partials, std):
 
     GST = np.zeros(partials.shape + (partials.shape[3],))
