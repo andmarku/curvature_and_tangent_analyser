682f11270adf7cc31f22cf7944efc23318b92190
snusstofilen <hannouch@student.chalmers.se>
2019-11-29 03:47:01 +0100

    new test for multiprocessing/threading

--- Test/threadingtest.py
+++ Test/threadingtest.py
@@ -1,22 +1,65 @@
+
+# Multithreading pool function
 from multiprocessing.pool import ThreadPool
 
-import numpy as np
+# Multiprocessing pool function
+from multiprocessing.pool import Pool
 
-def thread(matrix):
-    print("matrix:", matrix)
-    return matrix * matrix
+import numpy as np
+import time
+
+''' CPU lighter function '''
+def light(matrix):
+    result = matrix * matrix
+    return result
+
+''' CPU heavier function '''
+def heavy(matrix):
+    result = np.zeros(matrix.shape)
+    for (i,j), m in np.ndenumerate(matrix):
+        result[i,j] = m + i + j
+    return result
 
 if __name__ == '__main__':
 
-    data = np.random.rand(10,10,10)
+    data = np.random.rand(100,100,100)
 
-    print("data:", data)
+    time1 = time.time()
 
-    pool = ThreadPool(10)
-
-    result = pool.map(thread, data)
-
+    pool = ThreadPool(5)
+    result = pool.map(light, data)
     pool.close()
     pool.join()
 
-    print("result:", result)
+    time2 = time.time()
+
+    pool = Pool(5)
+    result = pool.map(light, data)
+    pool.close()
+    pool.join()
+
+    time3 = time.time()
+
+    print("--- CPU light function ---")
+    print("threading time elapsed:", time2 - time1)
+    print("processing time elapsed:", time3 - time2)
+
+    time1 = time.time()
+
+    pool = ThreadPool(5)
+    result = pool.map(heavy, data)
+    pool.close()
+    pool.join()
+
+    time2 = time.time()
+
+    pool = Pool(5)
+    result = pool.map(heavy, data)
+    pool.close()
+    pool.join()
+
+    time3 = time.time()
+
+    print("--- CPU heavy function ---")
+    print("threading time elapsed:", time2 - time1)
+    print("processing time elapsed:", time3 - time2)
