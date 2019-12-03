bfc64096c8d760fda65cefe67eeb2055b7b00bc5
snusstofilen <hannouch@student.chalmers.se>
2019-12-03 09:08:33 +0100

    Printing tangents also now

--- Test/writeCurvature.py
+++ Test/writeCurvature.py
@@ -3,7 +3,7 @@ from vtk.util import numpy_support as VN
 import vtk
 from vtk import vtkStructuredPointsWriter
 
-def writeNow(input):
+def writeNow(input, filename):
 
     dimensions = input.shape
 
@@ -13,19 +13,17 @@ def writeNow(input):
     data.AddArray(array)
 
     points = vtk.vtkStructuredPoints()
-    points.SetFieldData(data)
     points.SetDimensions(dimensions)
+    points.SetFieldData(data)
 
     writer = vtkStructuredPointsWriter()
 
-    writer.SetFileName('testwritefile.vtk')
+    writer.SetFileName(filename)
 
     writer.SetInputData(points)
     writer.Write()
 
     #stream = writer.OpenVTKFile()
-
-    import pdb; pdb.set_trace()
 
     #data.SetDimensions(dimensions)
 
@@ -35,9 +33,46 @@ def writeNow(input):
 
     #import pdb; pdb.set_trace()
 
+def writeTest(input, filename):
+
+    dimensions = input[:,:,:,0].shape
+
+    array1 = VN.numpy_to_vtk(input[:,:,:,0].ravel(), deep=True, array_type=vtk.VTK_FLOAT)
+    array2 = VN.numpy_to_vtk(input[:,:,:,1].ravel(), deep=True, array_type=vtk.VTK_FLOAT)
+    array3 = VN.numpy_to_vtk(input[:,:,:,2].ravel(), deep=True, array_type=vtk.VTK_FLOAT)
+
+    data = vtk.vtkPointData()
+    data.AddArray(array1)
+    data.AddArray(array2)
+    data.AddArray(array3)
+
+    points = vtk.vtkStructuredPoints()
+    points.SetDimensions(dimensions)
+    points.SetFieldData(data)
+
+    writer = vtkStructuredPointsWriter()
+
+    writer.SetFileName(filename)
+
+    writer.SetInputData(points)
+    writer.Write()
+
+    #stream = writer.OpenVTKFile()
+
+    #data.SetDimensions(dimensions)
+
+    #stream.WritePoints(data)
+
+    print(data)
+
+    #import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
 
     A = np.random.rand(10,10,10)
 
-    writeNow(A)
+    writeNow(A, 'curvature.vtk')
+
+    B = np.random.rand(10,10,10,3)
+
+    writeTest(B, 'tangent.vtk')
