79ef66b06a02543d1f9b4fcd267fbd8dc9b818f3
Fällman <andmarku@student.chalmers.se>
2019-12-16 16:10:08 +0100

    finished first version of curvatureCalculator

---
+++ curvature/curvatureWrapper.py
@@ -0,0 +1,9 @@
+import knutsson
+import dw_dt_approximator as dm
+import curvatureCalculator
+
+def mainCurvatureMethod(eigenvector, tensor_tangents):
+    tensor_knutVecs = knutsson.knutssonMapping(eigenvector)
+    tensor_dmAlongTangent = dmDotTangents(tensor_knutVecs,tensor_tangents)
+    tensor_curvatures = curvatureCalcForAllVoxels(tensor_dmAlongTangent)
+    return tensor_curvatures
