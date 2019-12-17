0270c9f3eea89bf7c6364d5f1838e7ef263a2f75
Fällman <andmarku@student.chalmers.se>
2019-12-17 12:21:44 +0100

    fixed small error

--- application/curvatures/curvatureWrapper.py
+++ application/curvatures/curvatureWrapper.py
@@ -2,8 +2,8 @@ import knutsson
 import dw_dt_approximator as dm
 import curvatureCalculator
 
-def curvatures(eigenvector, tensor_tangents):
-    tensor_knutVecs = knutsson.knutssonMapping(eigenvector)
+def curvatures(tensor_tangents):
+    tensor_knutVecs = knutsson.knutssonMapping(tensor_tangents)
     tensor_dmAlongTangent = dmDotTangents(tensor_knutVecs,tensor_tangents)
     tensor_curvatures = curvatureCalcForAllVoxels(tensor_dmAlongTangent)
     return tensor_curvatures
