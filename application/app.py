e06e6389a0d2f912a77883bdb88b2869fbdda170
MikaelLennefors <miklen@student.chalmers.se>
2020-01-10 16:55:38 +0100

    Uppdate

--- application/app.py
+++ application/app.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     filename = str(sys.argv[1])
     fibrewidth = int(sys.argv[2])
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         curvature = sys.argv[3]
         programWrapper.program(filename, fibrewidth, curvature)
     else:
