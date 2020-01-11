5bd235a48777946e4e4b4e3c16922ab9c1fdd06d
MikaelLennefors <miklen@student.chalmers.se>
2020-01-11 14:29:18 +0100

    Fick fucking quiver indexing

--- application/analysers/plotTangents.py
+++ application/analysers/plotTangents.py
@@ -5,11 +5,11 @@ import numpy as np
 
 def plotTangentComponents(tensor_tangents):
     dims =  tensor_tangents.shape
-    # x,y,z = np.meshgrid(np.arange(0,dims[0],1),np.arange(0,dims[1],1),np.arange(0,dims[2],1))
-    x = tensor_tangents[:,:,:,0].shape[0]
-    y = tensor_tangents[:,:,:,1].shape[1]
-    z = tensor_tangents[:,:,:,2].shape[2]
-    X,Y,Z = np.meshgrid(np.arange(0,x),np.arange(0,y),np.arange(0,z))
+    x,y,z = np.meshgrid(np.arange(0,dims[0],1),np.arange(0,dims[1],1),np.arange(0,dims[2],1), indexing = 'ij')
+    # x = tensor_tangents[:,:,:,0].shape[0]
+    # y = tensor_tangents[:,:,:,1].shape[1]
+    # z = tensor_tangents[:,:,:,2].shape[2]
+    # X,Y,Z = np.meshgrid(np.arange(0,x),np.arange(0,y),np.arange(0,z), indexing = 'ij')
 
     xcomp = tensor_tangents[:,:,:,0]
     ycomp = tensor_tangents[:,:,:,1]
@@ -22,7 +22,7 @@ def plotTangentComponents(tensor_tangents):
     plt.ylabel('y')
 
     # Plot x xomponents only
-    ax.quiver(X,Y,Z, xcomp, ycomp, zcomp, alpha=0.8)
+    ax.quiver(x,y,z, xcomp, ycomp, zcomp, alpha=0.8)
     fig.set_size_inches(16, 9)
     # plt.savefig("tangents.png")
     plt.show()
