2bd6427f27d65c1ab200510d430f442582e10773
Fällman <andmarku@student.chalmers.se>
2020-01-12 14:15:30 +0100

    made sure that also eigh takes geometric multiplicity in account

--- application/tangents/eigenVectors/eighCalculator.py
+++ application/tangents/eigenVectors/eighCalculator.py
@@ -11,13 +11,34 @@ def calculateWithEigh(GST):
             eigs, vecs = np.linalg.eigh(GST[i,j,k,:,:])
 
             index = np.argmin(abs(eigs))
+
+            # make sure that the zero vector is returned if the geometric
+            # multiplicity is larger than one
             eigVal = eigs[index]
             alternativeEigVals = np.delete(eigs, index)
             if(eigVal == alternativeEigVals[0]):
-                data[i,j,k,:] = np.zeros(3)
+                egVec = vecs[:, index]
+                alternativeEigVecs = np.delete(vecs, index)
+                altEgVec = alternativeEigVecs[0]
+
+                # eigh returns normalized vectors, so dividing by norm is't needed
+                discrepancy =  1 - np.absolute(altEgVec.dot(egVec))
+                if(discrepancy > 1e-10):
+                    data[i,j,k,:] = np.zeros(3)
+                else:
+                    data[i,j,k,:] = vecs[:, index]
             elif(eigVal == alternativeEigVals[1]):
-                data[i,j,k,:] = np.zeros(3)
-            else:   
+                egVec = vecs[:, index]
+                alternativeEigVecs = np.delete(vecs, index)
+                altEgVec = alternativeEigVecs[1]
+
+                # eigh returns normalized vectors, so dividing by norm is't needed
+                discrepancy =  1 - np.absolute(altEgVec.dot(egVec))
+                if(discrepancy > 1e-10):
+                    data[i,j,k,:] = np.zeros(3)
+                else:
+                    data[i,j,k,:] = vecs[:, index]
+            else:
                 data[i,j,k,:] = vecs[:, index]
 
     print("finished eigenvalues")
